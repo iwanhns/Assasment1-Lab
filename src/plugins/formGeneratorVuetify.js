1c145cddeb91480d96ae4822a4379141f6a12278
Amaineku <i.sinanto.a@students.esqbs.ac.id>
2020-03-19 10:01:54 -0700

    update 1

--- src/plugins/formGeneratorVuetify.js
+++
@@ -1,7 +0,0 @@
-import Vue from 'vue'
-import FormGeneratorVuetify from 'form-generator-vuetify'
-
-Vue.use(FormGeneratorVuetify)
-
-// Add Our GraphQL CLient
-// export const graphqlFormClient = graphqlClient
